83b521f3d3c28c9865469b82573a7d70af6728cd
Oxana22 <nesterova.slt@gmail.com>
2022-10-02 20:39:36 +0300

    Цикл в цикле с проверкой условия <=3

--- Control/Program.cs
+++ Control/Program.cs
@@ -1,3 +1,16 @@
 ﻿
  string[] array1 = {"2", "hello", "world"};
  string[] array2 = new string[array1.Length];
+
+    int count =0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <=3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+
+
+ 
